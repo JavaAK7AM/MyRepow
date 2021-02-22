15216c144e6ebf0970be480cc68c8f3df4688a8d
Akshay <Akshay@gmail.com>
2021-02-22 13:38:36 +0000

    File Shaped

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 pulbic class Test{
 
+
+
+
+   static int m1(int a, int b){
+           return a+b;
+
+
+   }
    public static void main(String[] args){
               
               System.out.println("Hello");
